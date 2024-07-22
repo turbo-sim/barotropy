a6f547202da81a2a53b3efae7f96a856a93f6fac
roagr <roagr@dtu.dk>
2024-07-22 17:57:08 +0200

    update solver package

--- barotropy/__init__.py
+++ barotropy/__init__.py
@@ -19,6 +19,51 @@ from .plot_options import *
 from .sCO2_utilities import *
 from .utilities import *
 
-# Set plot options
-set_plot_options(minor_ticks=False)
+# Package info
+__version__ = "0.0.0"
+PACKAGE_NAME = "barotropy"
+URL_GITHUB = "https://github.com/turbo-sim/barotropy"
+URL_DOCS = "https://turbo-sim.github.io/barotropy/"
+URL_DTU = "https://thermalpower.dtu.dk/"
+BREAKLINE = 80 * "-"
+
+def print_banner():
+    """Prints a banner."""
+    banner = """
+                ____                   __                        
+               / __ )____ __________  / /__________  ____  __  __
+              / __  / __ `/ ___/ __ \/ __/ ___/ __ \/ __ \/ / / /
+             / /_/ / /_/ / /  / /_/ / /_/ /  / /_/ / /_/ / /_/ / 
+            /_____/\__,_/_/   \____/\__/_/   \____/ .___/\__, /  
+                                                 /_/    /____/   
+"""
+#     banner = """
+#             ____  ___    ____  ____  __________  ____  ______  __
+#            / __ )/   |  / __ \/ __ \/_  __/ __ \/ __ \/ __ \ \/ /
+#           / __  / /| | / /_/ / / / / / / / /_/ / / / / /_/ /\  / 
+#          / /_/ / ___ |/ _, _/ /_/ / / / / _, _/ /_/ / ____/ / /  
+#         /_____/_/  |_/_/ |_|\____/ /_/ /_/ |_|\____/_/     /_/   
+                                                                
+# """
+    print(BREAKLINE)
+    print(banner)
+    print(BREAKLINE)
+    # https://manytools.org/hacker-tools/ascii-banner/
+    # Style: Sland
+
+
+def print_package_info():
+    """Prints package information with predefined values."""
+
+    info = f""" Version:       {__version__}
+ Repository:    {URL_GITHUB}
+ Documentation: {URL_DOCS}"""
+    print_banner()
+    print(BREAKLINE)
+    print(info)
+    print(BREAKLINE)
+
+
+
+
 
