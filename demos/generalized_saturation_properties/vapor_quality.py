5198e5d0a4fbc48430765aa305d2edb405a0fb9d
roagr <roagr@dtu.dk>
2024-07-25 14:42:49 +0200

    add demos for generalized quality, superheating, subcooling

--- demos/generalized_saturation_properties/vapor_quality.py
+++ demos/generalized_saturation_properties/vapor_quality.py
@@ -8,12 +8,18 @@ import barotropy as bpy
 
 # Create the folder to save figures
 bpy.set_plot_options()
-fig_dir = "figures"
+fig_dir = "output"
 if not os.path.exists(fig_dir):
     os.makedirs(fig_dir)
 
 # Create fluid
-fluid = bpy.Fluid(name="CO2", exceptions=True)
+fluid = bpy.Fluid(
+    name="CO2",
+    exceptions=True,
+    generalize_quality=True,
+    compute_subcooling=True,
+    compute_superheating=True,
+)
 
 
 # --------------------------------------------------------------------------- #
@@ -38,25 +44,25 @@ s_array = np.linspace(s1 + delta_s / 8, s2 + delta_s / 16, 100)
 
 # Subcritical cases
 p_array = np.asarray([0.5, 0.6, 0.7, 0.8, 0.9, 0.99]) * fluid.critical_point.p
-states = bpy.compute_properties_meshgrid(fluid, bpy.PSmass_INPUTS, p_array, s_array, generalize_quality=True)
+states = bpy.compute_property_grid(fluid, bpy.PSmass_INPUTS, p_array, s_array)
 colormap = cm.magma(np.linspace(0.1, 0.7, len(p_array)))
 for i in range(states[prop_x].shape[-1]):
     ax1.plot(
         states[prop_x][:, i],
         states[prop_y][:, i],
         color=colormap[i],
-        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$",
     )
     ax2.plot(
         states[prop_x][:, i],
         states["Q"][:, i],
         color=colormap[i],
-        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$",
     )
 
 # Supercritical cases
 p_array = np.asarray([1.01, 1.2, 1.4, 1.6, 1.8, 2.0]) * fluid.critical_point.p
-states = bpy.compute_properties_meshgrid(fluid, bpy.PSmass_INPUTS, p_array, s_array, generalize_quality=True)
+states = bpy.compute_property_grid(fluid, bpy.PSmass_INPUTS, p_array, s_array)
 colormap = cm.magma(np.linspace(0.7, 0.1, len(p_array)))
 for i in range(states[prop_x].shape[-1]):
     ax1.plot(
@@ -64,14 +70,14 @@ for i in range(states[prop_x].shape[-1]):
         states[prop_y][:, i],
         color=colormap[i],
         linestyle="--",
-        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$",
     )
     ax2.plot(
         states[prop_x][:, i],
         states["Q"][:, i],
         color=colormap[i],
         linestyle="--",
-        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$p/p_{{crit}}={p_array[i]/fluid.critical_point.p:0.2f}$",
     )
 
 # Plot phase diagram
@@ -86,12 +92,12 @@ fluid.plot_phase_diagram(
 ax1.legend(loc="upper left", fontsize=10)
 ax2.legend(loc="upper left", fontsize=10)
 fig.tight_layout(pad=2)
-bpy.savefig_in_formats(fig, os.path.join(fig_dir, "extended_quality_isobars"))
+bpy.savefig_in_formats(fig, os.path.join(fig_dir, "generalized_vapor_quality_isobars"))
 
 # p_array1 = np.asarray(np.linspace(0.5, 0.99, 100)) * fluid.critical_point.p
 # p_array2 = np.asarray(np.linspace(1.01, 2.00, 100)) * fluid.critical_point.p
 # p_array = np.concatenate([p_array1, p_array2])
-# states = bpy.compute_properties_meshgrid(fluid, bpy.PSmass_INPUTS, p_array, s_array, generalize_quality=True)
+# states = bpy.compute_properties_meshgrid(fluid, bpy.PSmass_INPUTS, p_array, s_array)
 # contour = ax1.contour(
 #     states[prop_x],
 #     states[prop_y],
@@ -124,39 +130,39 @@ s_array = np.linspace(s1 + delta_s / 8, s2 + delta_s / 16, 100)
 # Subcritical cases
 T_array = np.asarray([0.75, 0.8, 0.85, 0.9, 0.95, 0.99]) * fluid.critical_point.T
 colormap = cm.magma(np.linspace(0.1, 0.7, len(T_array)))
-states = bpy.compute_properties_meshgrid(fluid, bpy.SmassT_INPUTS, s_array, T_array, generalize_quality=True)
+states = bpy.compute_property_grid(fluid, bpy.SmassT_INPUTS, s_array, T_array)
 for i in range(states[prop_x].shape[0]):
     ax1.plot(
         states[prop_x][i, :],
         states[prop_y][i, :],
         color=colormap[i],
-        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.T:0.2f}$",
     )
     ax2.plot(
         states[prop_x][i, :],
         states["Q"][i, :],
         color=colormap[i],
-        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.T:0.2f}$",
     )
 
 # Supercritical cases
 T_array = np.asarray([1.01, 1.1, 1.2, 1.3, 1.4, 1.5]) * fluid.critical_point.T
 colormap = cm.magma(np.linspace(0.7, 0.1, len(p_array)))
-states = bpy.compute_properties_meshgrid(fluid, bpy.SmassT_INPUTS, s_array, T_array, generalize_quality=True)
+states = bpy.compute_property_grid(fluid, bpy.SmassT_INPUTS, s_array, T_array)
 for i in range(states[prop_x].shape[0]):
     ax1.plot(
         states[prop_x][i, :],
         states[prop_y][i, :],
         color=colormap[i],
         linestyle="--",
-        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.T:0.2f}$",
     )
     ax2.plot(
         states[prop_x][i, :],
         states["Q"][i, :],
         color=colormap[i],
         linestyle="--",
-        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.p:0.2f}$ bar",
+        label=f"$T/T_{{crit}}={T_array[i]/fluid.critical_point.T:0.2f}$",
     )
 
 # Plot phase diagram
@@ -171,7 +177,7 @@ fluid.plot_phase_diagram(
 ax1.legend(loc="upper left", fontsize=10)
 ax2.legend(loc="upper left", fontsize=10)
 fig.tight_layout(pad=2)
-bpy.savefig_in_formats(fig, os.path.join(fig_dir, "extended_quality_isotherms"))
+bpy.savefig_in_formats(fig, os.path.join(fig_dir, "generalized_vapor_quality_isotherms"))
 
 
 # --------------------------------------------------------------------------- #
@@ -195,40 +201,39 @@ T_array = np.linspace(0.75, 1.5, 101) * fluid.critical_point.T
 # Liquid-like cases
 s_array = np.asarray([0.7, 0.8, 0.9, 0.99]) * fluid.critical_point.s
 colormap = cm.magma(np.linspace(0.1, 0.7, len(s_array)))
-states = bpy.compute_properties_meshgrid(fluid, bpy.SmassT_INPUTS, s_array, T_array, generalize_quality=True)
+states = bpy.compute_property_grid(fluid, bpy.SmassT_INPUTS, s_array, T_array)
 for i in range(states[prop_x].shape[1]):
     ax1.plot(
         states[prop_x][:, i],
         states[prop_y][:, i],
         color=colormap[i],
-        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$ bar",
+        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$",
     )
     ax2.plot(
         states[prop_y][:, i],
         states["Q"][:, i],
         color=colormap[i],
-        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$ bar",
+        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$",
     )
 
 # Gas-like cases
 s_array = np.asarray([1.01, 1.1, 1.2, 1.3]) * fluid.critical_point.s
 colormap = cm.magma(np.linspace(0.7, 0.1, len(s_array)))
-states = bpy.compute_properties_meshgrid(fluid, bpy.SmassT_INPUTS, s_array, T_array, generalize_quality=True)
+states = bpy.compute_property_grid(fluid, bpy.SmassT_INPUTS, s_array, T_array)
 for i in range(states[prop_x].shape[1]):
-    print(states[prop_x][:, i])
     ax1.plot(
         states[prop_x][:, i],
         states[prop_y][:, i],
         color=colormap[i],
         linestyle="--",
-        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$ bar",
+        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$",
     )
     ax2.plot(
         states[prop_y][:, i],
         states["Q"][:, i],
         color=colormap[i],
         linestyle="--",
-        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$ bar",
+        label=f"$s/s_{{crit}}={s_array[i]/fluid.critical_point.s:0.2f}$",
     )
 
 # Plot phase diagram
@@ -243,7 +248,7 @@ fluid.plot_phase_diagram(
 ax1.legend(loc="upper left", fontsize=10)
 ax2.legend(loc="upper left", fontsize=10)
 fig.tight_layout(pad=2)
-bpy.savefig_in_formats(fig, os.path.join(fig_dir, "extended_quality_isentropes"))
+bpy.savefig_in_formats(fig, os.path.join(fig_dir, "generalized_vapor_quality_isentropes"))
 
 
 # Show figures
