8942f03a73267009b197c742bb94e277c4700998
roagr <roagr@dtu.dk>
2024-07-31 20:29:43 +0200

    Bump version: 0.0.2 → 0.0.3

--- barotropy/__init__.py
+++ barotropy/__init__.py
@@ -21,7 +21,7 @@ from .sCO2_utilities import *
 from .utilities import *
 
 # Package info
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 PACKAGE_NAME = "barotropy"
 URL_GITHUB = "https://github.com/turbo-sim/barotropy"
 URL_DOCS = "https://turbo-sim.github.io/barotropy/"
