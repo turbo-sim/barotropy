69bf89038138131981e4e37e756aec3f19e33a4e
roagr <roagr@dtu.dk>
2024-07-31 17:49:23 +0200

    update LAES turbine case

--- projects/LAES_turbine/LAES_turbine.py
+++ projects/LAES_turbine/LAES_turbine.py
@@ -19,115 +19,107 @@ if not os.path.isdir(DIR_OUT):
 # Load cases from Excel
 case_table = pd.read_excel("./simulation_cases.xlsx")
 case_indices = case_table["index"]
-# case_indices = [1, 2, 3]
-case_indices = [2]
+# case_indices = [1, 2]
 case_table = case_table[case_table["index"].isin(case_indices)]
 
 # Define plot settings
 var_x = "s"
 var_y = "T"
-n_points = None
-# colors = bpy.COLORS_MATLAB
-# colors = cm.Reds(np.linspace(0.5, 1.0, len(case_indices)))
-colors = cm.magma(np.linspace(0.2, 0.8, len(case_indices)))
-
 save_figures = True
+colors = cm.magma(np.linspace(0.2, 0.8, max(len(case_indices) - 1 , 1)))  # Reds, Blues, magma
 
 # Loop over cases
 for i, (idx, row) in enumerate(case_table.iterrows()):
 
-    # Create fluid object
-    fluid_name = row["fluid_name"]
+    # Process new row
     print(f"Processing case {i+1} of {len(case_table)}")
     print(row)
     print()
-    fluid = bpy.Fluid(name=fluid_name, backend="HEOS", exceptions=True)
+    color = "black" if i == 0 else colors[i - 1]
+    dir_out = os.path.join(DIR_OUT, row["tag"])
 
-    p_in = row["p0_in"]
-    p_out = row["p_out"]
-
-    # Evaluate barotropic model
-    polyfit = bpy.barotropic_model_one_fluid(
+    # Create barotropic model object
+    fluid_name = row["fluid_name"]
+    model = bpy.BarotropicModelOneComponent(
         fluid_name=fluid_name,
         T_in=row["T0_in"],
-        p_in=p_in,
-        p_out=p_out,
+        p_in=row["p0_in"],
+        p_out=row["p_out"],
         efficiency=row["polytropic_efficiency"],
-        calculation_type="blending",
+        calculation_type=row["calculation_type"],
         blending_onset=row["q_onset"],
         blending_width=row["q_transition"],
-        n_eval=n_points,
         HEOS_solver="hybr",
-        ODE_solver="BDF",
+        ODE_solver="LSODA",
         ODE_tolerance=1e-9,
-        polynomial_order=8,
-        polynomial_vars=["density", "viscosity", "speed_sound"],
+        polynomial_degree=8,
+        polynomial_format="horner",
+        output_dir=dir_out,
     )
 
-    # # Create figure for plotting
-    # if i == 0:
-    #     # Plot phase diagram
-    #     fig_1, ax_1 = plt.subplots(figsize=(6.0, 5.0))
-    #     ax_1.set_xlabel("Entropy (J/kg/K)")
-    #     ax_1.set_ylabel("Temperature (K)")
-    #     # s_min, s_max = 1.3, 1.7
-    #     # T_min, T_max = 25, 35
-    #     # ax.set_xlim([s_min, s_max])
-    #     # ax.set_ylim([T_min, T_max])
-    #     ax_1 = fluid.plot_phase_diagram(
-    #         var_x,
-    #         var_y,
-    #         axes=ax_1,
-    #         plot_critical_point=True,
-    #         plot_saturation_line=True,
-    #         plot_spinodal_line=False,
-    #         plot_quality_isolines=True,
-    #     )
+    # Evaluate barotropic model and export polynomial expressions
+    model.compute_properties()
+    model.fit_polynomials()
+    model.export_expressions_fluent(output_dir=dir_out)
+    model.export_expressions_cfx(output_dir=dir_out)
+    # for var in model.poly_fitter.variables:
+    #     model.poly_fitter.plot_polynomial_and_error(var=var, savefig=True, showfig=False)
 
-    # if i == 0:
-    #     color = "black"
-    # else:
-    #     color = colors[i]
-        
-    # ax_1.plot(
-    #     states[var_x],
-    #     states[var_y],
-    #     linewidth=1.0,
-    #     # marker="o",
-    #     # markersize=3,
-    #     color=color,
-    #     label=rf"$q_\text{{onset}}={row['q_onset']:0.2f}$, {row['multiphase_model']}",
-    # )
+#     # Create figure for plotting
+#     if i == 0:
+#         # Plot phase diagram
+#         fluid = bpy.Fluid(name=fluid_name, backend="HEOS", exceptions=True)
+#         fig, (ax_1, ax_2) = plt.subplots(
+#             1, 2, figsize=(12.0, 5.0), gridspec_kw={"wspace": 0.25}
+#         )
+#         ax_1.set_xlabel("Entropy (J/kg/K)")
+#         ax_1.set_ylabel("Temperature (K)")
+#         # s_min, s_max = 1.3, 1.7
+#         # T_min, T_max = 25, 35
+#         # ax.set_xlim([s_min, s_max])
+#         # ax.set_ylim([T_min, T_max])
+#         ax_1 = fluid.plot_phase_diagram(
+#             var_x,
+#             var_y,
+#             axes=ax_1,
+#             plot_critical_point=True,
+#             plot_saturation_line=True,
+#             plot_spinodal_line=True,
+#             plot_quality_isolines=True,
+#             N=50,
+#         )
 
+#     ax_1.plot(
+#         model.states[var_x],
+#         model.states[var_y],
+#         # linewidth=1.0,
+#         # marker="o",
+#         # markersize=3,
+#         color=color,
+#         linewidth=1.25,
+#         label=rf"$q_\text{{onset}}={row['q_onset']:0.2f}$, {row['calculation_type']}",
+#     )
 
-    fig_2 = polyfit.plot_polynomial_and_error(var="density")
-    # fig_3 = poly.plot_polynomial_and_error(var="viscosity")
-    # fig_4 = poly.plot_polynomial_and_error(var="speed_sound")
+#     ax_2.plot(
+#         model.states["p"],
+#         model.states["rho"],
+#         # linewidth=1.0,
+#         marker="o",
+#         markersize=3,
+#         color=color,
+#         label=rf"$q_\text{{onset}}={row['q_onset']:0.2f}$, {row['calculation_type']}",
+#     )
 
-
-    polyfit.export_fluent_expressions()
-
-
-
-# # Adjust plot
+# # Add legend
 # ax_1.legend(loc="lower right")
 # ax_2.legend(loc="lower right")
 # # bpy.scale_graphics_x(fig, +1e-3, mode="multiply")
 # # bpy.scale_graphics_y(fig, -273.15, mode="add")
-# fig_1.tight_layout(pad=1)
-# fig_2.tight_layout(pad=1)
-
-
-# # export_fluent_expressions(barotropic_model, fluid_name=fluid_nane, case_name=case_name, output_dir=output_folder)
-# # export_cfx_expressions(barotropic_model, fluid_name=fluid_nane, case_name=case_name, output_dir=output_folder)
-
 
 # # Save figures
 # if save_figures:
-#     bpy.savefig_in_formats(
-#         fig_1, os.path.join(DIR_OUT, f"barotropic_model_{var_x}_{var_y}_diagram")
-#     )
-#     bpy.savefig_in_formats(fig_2, os.path.join(DIR_OUT, "barotropic_model_rho_p"))
+#     file_path = os.path.join(DIR_OUT, f"barotropic_model_{var_x}_{var_y}_diagram")
+#     bpy.savefig_in_formats(fig, file_path)
 
-# Show figure
-plt.show()
+# # Show figure
+# plt.show()
--- projects/LAES_turbine/notes.md
+++ projects/LAES_turbine/notes.md
@@ -1,8 +1,11 @@
 
 
+## LAES Turbine notes
 
-The spinodal line follows the line q=0.1 more or less closely
-
-The cases with q_onset from 0.00 to 0.08 cover all relevant cases.
-
-I have to fit polynomisl to the datapoints
+- The spinodal line of nitrogen approximately follows the line of 10% vapor quality
+- I computed the barotropic model properties for different values of phase-change t onset ranging from 0% to 8% (before spinodal) as detailed in the Excel file
+- I generated fitted polynomials for these properties
+  - The degree of the polynomial for the transition region should be relatively small (maybe 4).
+  - Otherwise there could be numerical roundoff problems in CFX/Fluent (is it because of single-precision?)
+  - The degree of the polynomials for the single-phase and equilibrium regions can be high to achieve good agreement with CoolProp data
+- Both Fluent and CFX expressions are working (Wednesday 31-07-2024)
