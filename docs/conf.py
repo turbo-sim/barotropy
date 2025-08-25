05a60b7907e8b44e6af98dc3aebce97d664825c7
roagr <roagr@dtu.dk>
2025-08-25 19:21:08 +0200

    Bump version: 0.2.16 → 0.2.17

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "barotropy"
 copyright = "2024, Sustainable Thermal Power DTU"
 author = "Roberto Agromayor, Simone Parisi, Amit Kumar, Fredrik Haglind"
-release = "v0.2.16"
+release = "v0.2.17"
 
 # Define extensions
 extensions = [
